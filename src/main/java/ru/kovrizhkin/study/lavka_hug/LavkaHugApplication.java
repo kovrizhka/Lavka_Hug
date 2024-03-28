b480d4002f079ade86c371bd54a49ed2f4286222
kovrizhka <pivsru@gmail.com>
2024-03-28 20:03:39 +0500

    initialization commit

---
+++ src/main/java/ru/kovrizhkin/study/lavka_hug/LavkaHugApplication.java
@@ -0,0 +1,13 @@
+package ru.kovrizhkin.study.lavka_hug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavkaHugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LavkaHugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kovrizhkin/study/lavka_hug/LavkaHugApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kovrizhkin.study.lavka_hug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavkaHugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
