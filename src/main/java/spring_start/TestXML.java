3de768edff97038f5fae6155382d4a1758285847
MrSilver <pivsru@gmail.com>
2024-06-25 20:23:02 +0500

    удалил какую-то шляпу 2

--- src/main/java/spring_start/TestXML.java
+++ src/main/java/spring_start/TestXML.java
@@ -6,12 +6,7 @@ import java.util.stream.IntStream;
 
 public class TestXML {
 
-    private static final int FROM_NUMBER_FIRST_THREAD = 1;
-    private static final int TO_NUMBER_FIRST_THREAD = 500;
-    private static final int FROM_NUMBER_SECOND_THREAD = 501;
-    private static final int TO_NUMBER_SECOND_THREAD = 1000;
 
-    private static final String TEMPLATE = "%s : %d\n";
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
