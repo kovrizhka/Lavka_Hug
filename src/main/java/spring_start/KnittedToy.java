8e170ca9fc444bac7517025b2003b8bf480cd4ec
MrSilver <pivsru@gmail.com>
2024-06-30 11:59:58 +0500

    DI через сеттер

--- src/main/java/spring_start/KnittedToy.java
+++ src/main/java/spring_start/KnittedToy.java
@@ -3,6 +3,6 @@ package spring_start;
 public class KnittedToy implements Souvenir {
     @Override
     public void makeHappy() {
-        System.out.println("Я мягкая и я радую чебурека!!!");
+        System.out.println("Я мягкая и я радую человека!!!");
     }
 }
--- src/main/java/spring_start/ShoppingCart.java
+++ src/main/java/spring_start/ShoppingCart.java
@@ -2,14 +2,24 @@ package spring_start;
 
 public class ShoppingCart {
 
-    private final Souvenir souvenir;
+    private Souvenir souvenir;
 
     public ShoppingCart(Souvenir souvenir) {
         this.souvenir = souvenir;
+    }
+
+    public ShoppingCart() {
+        System.out.println("ShoppingCart создана!");
     }
 
     public void buySouvenir() {
         System.out.println("Покупаю!");
         souvenir.makeHappy();
     }
+
+    public void setSouvenir(Souvenir souvenir) {
+        this.souvenir = souvenir;
+        System.out.println("В корзину добавлен: " + this.souvenir);
+    }
+
 }
